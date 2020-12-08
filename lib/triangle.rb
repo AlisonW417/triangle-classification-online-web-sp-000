eafb25359943806f7b537f37225fb7c0f910ad6e
Alison Williams <apw5011@gmail.com>
2020-12-08 19:51:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,6 +18,10 @@ class Triangle
       begin 
         raise TriangleError
       end 
+    elsif @side1 + @side2 < @side3
+      begin
+        raise TriangleError
+      end 
     elsif @side1 == @side2 && @side2 == @side3
       :equilateral
     elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
