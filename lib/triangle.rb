3772022edfe9e9a7c6401207c7051332732187c3
Alison Williams <apw5011@gmail.com>
2020-12-08 19:48:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,8 +21,6 @@ class Triangle
     elsif @side1 == 0 && @side2 == 0 && @side3 == 0 
       begin 
         raise TriangleError
-        rescue TriangleError => error
-          puts message.error
       end 
     else 
       :isosceles
