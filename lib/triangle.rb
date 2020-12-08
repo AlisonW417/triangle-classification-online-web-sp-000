a32c65538d14bb667d83744037f86eb843c6bb19
Alison Williams <apw5011@gmail.com>
2020-12-08 19:25:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,8 @@ class Triangle
       :equilateral
     elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
       :scalene
+    elsif @side1 == 0 || @side2 == 0 || @side3 == 0 
+      begin 
     else 
       :isosceles
     end 
